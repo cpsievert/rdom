32042dcba6b15a040232e2db1b540f3de2dd06b6
Carson Sievert <cpsievert1@gmail.com>
2015-05-21 13:51:04 -0500

    Allow user to specify timeout duration

--- R/rdom.R
+++ R/rdom.R
@@ -1,28 +1,33 @@
 #' Return DOM of a website as HTML
 #'
 #' @param url a URL of a website.
+#' @param timeout maximum time to wait for page to load and render, in seconds
 #' @export
 #' @examples
 #'
 #' library("rvest")
+#' # doesn't work
 #' html("http://www.techstars.com/companies/stats/") %>%
 #'   html_node(".table75") %>% html_table()
-#' t <- rdom("http://www.techstars.com/companies/stats/")
-#' t %>% html_node(".table75") %>% html_table()
-#' rdom("http://www.techstars.com/companies/stats/") %>% html_node(".table75") %>% html_table()
-#' rdom("http://www.google.org/")
+#' # should work
+#' rdom("http://www.techstars.com/companies/stats/") %>%
+#'   html_node(".table75") %>% html_table()
 #'
 
-rdom <- function(url) {
+rdom <- function(url, timeout=10) {
   if (missing(url)) stop("Please specify a url.")
   args <- list(
     paste0("'", system.file("rdom.js", package = "rdom"), "'"),
-    url
+    url,
+    timeout*1000
   )
   phantom_bin <- find_phantom()
   res <- system2(phantom_bin, args = as.character(args),
                  stdout = TRUE, stderr = TRUE, wait = TRUE)
   st <- attr(res, "status")
-  if (!is.null(st)) stop("phantomjs returned failure value: ", st)
+  if (!is.null(st)) {
+    message("Uh oh, something went wrong :", paste(res, "\n"))
+    stop("phantomjs returned failure value: ", st)
+  }
   XML::htmlParse(res, asText = TRUE)
 }
--- inst/rdom.js
+++ inst/rdom.js
@@ -1,14 +1,24 @@
+// TODO: include options to manipulate DOM and/or inject JS?
+var page = require('webpage').create();
 // TODO: provide a check for argument usage
 var args = require('system').args;
-var page = require('webpage').create();
 var url = args[1];
+var timeout = args[2];
 
 page.open(url, function (status) {
-  // TODO: include options to manipulate DOM and/or inject JS?
-  // http://phantomjs.org/page-automation.html
-    var doc = page.evaluate(function () {
-        return document.documentElement.outerHTML;
-    });
-    console.log(doc);
+  if (status !== 'success') {
+    console.log("phantomjs couldn't open the page.");
     phantom.exit();
+  } else {
+    // Get phantomjs 10 seconds to evaluate the page
+    // http://stackoverflow.com/questions/28950627/settimeout-in-phantom-js
+    setTimeout(function() {
+      var doc = page.evaluate(function () {
+        return document.documentElement.outerHTML;
+      });
+      console.log(doc);
+      phantom.exit();
+    }, timeout);
+  }
 });
+
